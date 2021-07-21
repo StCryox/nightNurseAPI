ae240dff75dcf0b30429148ea0942c41f093a26f
amaurygrillot <39194528+amaurygrillot@users.noreply.github.com>
2021-07-22 01:11:26 +0200

    Ajout gestion CORS

--- index.ts
+++ index.ts
@@ -3,12 +3,19 @@ config();
 import express, {Express} from "express";
 import bodyParser from "body-parser";
 
+
 import {buildRoutes} from "./api/routes";
-
+const cors = require('cors');
 const app: Express = express();
-
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200, // For legacy browser support
+    methods: "GET, PUT, POST, DELETE"
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 buildRoutes(app);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
